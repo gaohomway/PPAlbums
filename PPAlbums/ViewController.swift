3244e57fc348ee9bf380ce1ddb64ec39acd0c35d
weifans <duanhaisi@gmail.com>
2015-12-18 17:10:29 +0800

    修改了Paipou相册滑动模块

--- PPAlbums/ViewController.swift
+++ PPAlbums/ViewController.swift
@@ -8,22 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let bound = UIScreen.mainScreen().bounds
+let boundW = bound.size.width
+let boundH = bound.size.height
+
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     var scrollView: UIScrollView!
-    
+    var pageControl: UIPageControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "PPAlbums"
+        self.automaticallyAdjustsScrollViewInsets = false
         self.albumsScrollView()
+        
+        let paipou = UILabel(frame: CGRectMake(0, boundH-50, boundW, 50))
+        paipou.textAlignment = .Center
+        paipou.text = "www.paipou.com"
+        self.view.addSubview(paipou)
     }
 
     func albumsScrollView() {
-        
-        scrollView = UIScrollView(frame: self.view.frame)
+
+        scrollView = UIScrollView(frame: CGRectMake(0, 64, boundW, boundW))
+        scrollView.delegate = self
         scrollView.pagingEnabled = true
-        scrollView.backgroundColor = UIColor.whiteColor()
+        scrollView.backgroundColor = UIColor(red:0.93, green:0.93, blue:0.93, alpha:1)
+        scrollView.showsHorizontalScrollIndicator = false
         view.addSubview(scrollView)
         
         self.albumsData()
@@ -38,24 +50,54 @@ class ViewController: UIViewController {
     func photoPage(pageImages: Array<UIImage>) {
         
         let dx:CGFloat = 20
-        let itemWidth = scrollView.bounds.width + dx * CGFloat(2.0)
+        let itemWidth = boundW + dx * CGFloat(2.0)
         
         for  i in 0..<pageImages.count {
+            
             var frame = scrollView.bounds
             frame.origin.x = itemWidth * CGFloat(i)
             frame.origin.y = 0.0
             frame.size.width = itemWidth
+            
             let newPageView = UIImageView(image: pageImages[i])
             newPageView.contentMode = UIViewContentMode.ScaleAspectFit
             newPageView.frame = CGRectInset(frame, dx, 0)
             scrollView.addSubview(newPageView)
         }
-        let tt = view.bounds.size
-        scrollView.frame = CGRect(x: -dx, y: 0, width: itemWidth, height: tt.height)
+        scrollView.frame = CGRect(x: -dx, y: 64, width: itemWidth, height: itemWidth)
         let pagesScrollViewSize = scrollView.frame.size
         scrollView.contentSize = CGSizeMake(itemWidth * CGFloat(pageImages.count), pagesScrollViewSize.height)
         
+        self.initAccountPageControl()
     }
+    
+    func initAccountPageControl() {
+        pageControl = UIPageControl()
+        pageControl.frame = CGRect(x: 0, y: boundW+64, width: boundW, height: 50)
+        pageControl.numberOfPages = 4
+        pageControl.currentPage = 0
+        pageControl.currentPageIndicatorTintColor = UIColor.whiteColor()
+        pageControl.pageIndicatorTintColor = UIColor.grayColor()
+        self.view.addSubview(pageControl)
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        
+        let xOffset: CGFloat = scrollView.contentOffset.x
+        print(xOffset)
+        
+        
+        if (xOffset < 1.0) {
+            pageControl.currentPage = 0
+        } else if (xOffset < self.view.bounds.width + 1) {
+            pageControl.currentPage = 1
+        } else if (xOffset < (self.view.bounds.width*2) + 1) {
+            pageControl.currentPage = 2
+        }else{
+            pageControl.currentPage = 3
+        }
+    }
+
     
     
     override func didReceiveMemoryWarning() {
